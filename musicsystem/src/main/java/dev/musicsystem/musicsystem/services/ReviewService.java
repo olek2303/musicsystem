31e82a79bd5c9d3f5fdc64132c26303ee644c86f
olek2303 <olek2303@gmail.com>
2024-04-20 11:14:35 +0200

    working still with crud

--- musicsystem/src/main/java/dev/musicsystem/musicsystem/services/ReviewService.java
+++ musicsystem/src/main/java/dev/musicsystem/musicsystem/services/ReviewService.java
@@ -1,9 +1,12 @@
 package dev.musicsystem.musicsystem.services;
 
 import dev.musicsystem.musicsystem.entity.Review;
+import dev.musicsystem.musicsystem.entity.User;
 import dev.musicsystem.musicsystem.repositories.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ReviewService {
@@ -11,5 +14,8 @@ public class ReviewService {
     @Autowired
     private ReviewRepository reviewRepository;
 
+    public List<Review> AllReviews() {
+        return reviewRepository.findAll();
+    }
 
 }
