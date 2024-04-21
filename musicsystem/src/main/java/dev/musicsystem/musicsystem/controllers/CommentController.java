01c2f701fb17f564c7e26a55e968b999c4650baa
olek2303 <olek2303@gmail.com>
2024-04-21 23:51:24 +0200

    updated comment api, need to finish review and user

--- musicsystem/src/main/java/dev/musicsystem/musicsystem/controllers/CommentController.java
+++ musicsystem/src/main/java/dev/musicsystem/musicsystem/controllers/CommentController.java
@@ -1,9 +1,14 @@
 package dev.musicsystem.musicsystem.controllers;
 
+import dev.musicsystem.musicsystem.entity.Album;
+import dev.musicsystem.musicsystem.entity.Comment;
 import dev.musicsystem.musicsystem.services.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/comment")
@@ -11,5 +16,59 @@ public class CommentController {
 
     @Autowired
     private CommentService commentService;
+
+    @GetMapping("/all")
+    public List<Comment> getAllComments() { return commentService.AllComments();}
+
+    @PostMapping("")
+    public ResponseEntity<Comment> addComment(@RequestBody Comment comment) {
+        if (commentService.commentExists(comment)) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+
+        Comment savedComment = commentService.saveComment(comment);
+
+        if (savedComment == null) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return new ResponseEntity<>(savedComment, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{commentId}")
+    public ResponseEntity<Comment> updateComment(@PathVariable Long commentId, @RequestBody Comment updatedComment) {
+        Comment existingComment = commentService.commentById(commentId);
+
+        if (existingComment == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        updatedComment.setCommentId(commentId);
+
+        Comment savedComment = commentService.saveComment(updatedComment);
+
+        if (savedComment == null) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return new ResponseEntity<>(savedComment, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{commentId}")
+    public ResponseEntity<Void> deleteComment(@PathVariable Long commentId) {
+        Comment existingComment = commentService.commentById(commentId);
+
+        if (existingComment == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        try {
+            commentService.deleteComment(commentId);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
 }
--- musicsystem/src/main/java/dev/musicsystem/musicsystem/services/CommentService.java
+++ musicsystem/src/main/java/dev/musicsystem/musicsystem/services/CommentService.java
@@ -12,7 +12,27 @@ public class CommentService {
     @Autowired
     private CommentRepository commentRepository;
 
+    public List<Comment> AllComments() { return commentRepository.findAll(); }
     public List<Comment> getCommentsByReviewId(Long reviewId) {
         return commentRepository.findByReviewReviewId(reviewId);
+    }
+
+    public boolean commentExists(Comment comment) { return commentRepository.existsById(comment.getCommentId()); }
+
+    public Comment saveComment(Comment comment) {
+        try {
+            return commentRepository.save(comment);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public void deleteComment(Long commentId) {
+        commentRepository.deleteById(commentId);
+    }
+
+    public Comment commentById(Long commentId) {
+        return commentRepository.findById(commentId).orElse(null);
     }
 }
