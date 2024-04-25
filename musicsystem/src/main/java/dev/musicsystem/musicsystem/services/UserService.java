c40cb22427e0213c29fbf8626bd4a5a4c000fba0
olek2303 <olek2303@gmail.com>
2024-04-25 18:13:40 +0200

    added user restapi, move to hibernate

--- musicsystem/src/main/java/dev/musicsystem/musicsystem/services/UserService.java
+++ musicsystem/src/main/java/dev/musicsystem/musicsystem/services/UserService.java
@@ -5,6 +5,8 @@ import dev.musicsystem.musicsystem.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
     @Autowired
@@ -14,4 +16,17 @@ public class UserService {
         return userRepository.findById(userId).orElse(null);
     }
 
+    public List<User> allUsers() { return userRepository.findAll(); }
+
+    public User addUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteById(Long userId) {
+        userRepository.deleteById(userId);
+    }
+
+    public boolean userExists(User user) {
+        return userRepository.existsById(user.getUserId());
+    }
 }
