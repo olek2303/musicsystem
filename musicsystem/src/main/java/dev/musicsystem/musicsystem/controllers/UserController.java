8d67203ad5cf3a3cd69fb0018f7a8be8dd1bdd15
olek2303 <olek2303@gmail.com>
2024-04-25 18:13:40 +0200

    added user restapi, move to hibernate

--- musicsystem/src/main/java/dev/musicsystem/musicsystem/controllers/UserController.java
+++ musicsystem/src/main/java/dev/musicsystem/musicsystem/controllers/UserController.java
@@ -1,9 +1,14 @@
 package dev.musicsystem.musicsystem.controllers;
 
+import dev.musicsystem.musicsystem.entity.Comment;
+import dev.musicsystem.musicsystem.entity.User;
 import dev.musicsystem.musicsystem.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -11,4 +16,54 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @GetMapping("/all")
+    public List<User> getAllUsers() {
+        return userService.allUsers();
+    }
+
+    @PostMapping("")
+    public ResponseEntity<User> addUser(User user) {
+        if (userService.userExists(user)) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+        User savedUser = userService.addUser(user);
+
+        if (savedUser == null) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{userId}")
+    public ResponseEntity<User> updateUser(@PathVariable Long userId, @RequestBody User updatedUser) {
+        User existingUser = userService.UserById(userId);
+
+        if (existingUser == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        updatedUser.setUserId(userId);
+
+        User savedUser = userService.addUser(updatedUser);
+
+        if (savedUser == null) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return new ResponseEntity<>(savedUser, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long userId) {
+        User existingUser = userService.UserById(userId);
+
+        if (existingUser == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        userService.deleteById(userId);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
