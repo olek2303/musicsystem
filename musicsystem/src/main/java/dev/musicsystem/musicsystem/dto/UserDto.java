37f24641518dfbba0a8413c7cb44b964047a4d91
olek2303 <olek230303@gmail.com>
2024-04-26 22:01:15 +0200

    added security, test it

---
+++ musicsystem/src/main/java/dev/musicsystem/musicsystem/dto/UserDto.java
@@ -0,0 +1,18 @@
+package dev.musicsystem.musicsystem.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto
+{
+    private Long userId;
+    private String username;
+    private String email;
+    private String password;
+}
