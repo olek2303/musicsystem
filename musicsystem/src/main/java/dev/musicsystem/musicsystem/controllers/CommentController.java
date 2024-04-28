b4ef1f30ae128b7d712b48e1d3206881055cc24b
olek2303 <olek2303@gmail.com>
2024-04-28 17:55:28 +0200

    changes with security, added posibility to post and get for everyone now

--- musicsystem/src/main/java/dev/musicsystem/musicsystem/controllers/CommentController.java
+++ musicsystem/src/main/java/dev/musicsystem/musicsystem/controllers/CommentController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/comment")
+@RequestMapping("/api/v1/comments")
 public class CommentController {
 
     @Autowired
