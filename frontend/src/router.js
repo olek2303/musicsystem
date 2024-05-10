8af3b4af1eaed2255c4721db05f827c490991dc3
olek2303 <olek2303@gmail.com>
2024-05-10 23:23:14 +0200

    rewrite getreviewbyalbumid to push also photoUrl (change album to albumdto with photourl)

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from './components/Home.vue'
 import Register from './components/RegistrationPanel.vue'
+import ReviewPage from "@/components/ReviewPage.vue";
 
 const routes = [
     { path: '/', component: Home },
     { path: '/home', component: Home },
-    { path: '/register', component: Register }
+    { path: '/register', component: Register },
+    { path: '/review/:id', name: 'review', component: ReviewPage, props: true }
 ]
 
 const router = createRouter({
