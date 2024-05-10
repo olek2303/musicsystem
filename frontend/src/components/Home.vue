198bf2bef6a569161af25fb1558172152d4e5379
olek2303 <olek2303@gmail.com>
2024-05-10 23:23:14 +0200

    rewrite getreviewbyalbumid to push also photoUrl (change album to albumdto with photourl)

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -7,13 +7,13 @@
   <div>
     <h1 class="text-xl flex justify-center text-center ml-3 sm:ml-0">Mainly reviews of rock albums and many of its subgenres.</h1>
     <div class="mb-36">
-      <h1 class="flex-1 mt-6 ml-3 sm:ml-20">Latest Reviews</h1>
+      <h2 class="flex-1 mt-6 ml-3 sm:ml-20">Latest Reviews</h2>
       <div class="flex flex-col sm:flex-row justify-center items-center">
-        <div v-for="album in albums" :key="album.id" class="relative justify-center m-6 max-w-52 sm:max-w-56">
-          <img :src="album.photoUrl" class="mr-3 rounded-2xl max-w-52 sm:max-w-56" >
+        <div v-for="album in albums" :key="album.albumId" class="relative justify-center m-6 max-w-52 sm:max-w-56 album" @click="goToReview(album.albumId)">
+          <img :src="album.photoUrl" :alt="album.title + ' by ' + album.artist" class="mr-3 rounded-2xl max-w-52 sm:max-w-56" >
           <div class="absolute bottom-0 flex flex-col text-left text-white bg-black/15 backdrop-blur-sm rounded-b-2xl left-0 inset-x-0">
             <p class="ml-2 mt-0.5 drop-shadow">{{ album.artist }}</p>
-            <h1 class="ml-2 mb-1 text-xl font-bold drop-shadow ">{{ album.title }}</h1>
+            <h2 class="ml-2 mb-1 text-xl font-bold drop-shadow ">{{ album.title }}</h2>
           </div>
         </div>
       </div>
@@ -38,9 +38,35 @@ export default {
       console.error(error);
     }
   },
+  methods: {
+    async goToReview(albumId) {
+      try {
+        if (!isNaN(albumId) && parseInt(albumId) > 0) {
+          const response = await axios.get(`http://localhost:8080/api/v1/albums/${albumId}`);
+          const reviewData = response.data;
+
+          await this.$router.push({ name: 'review', params: { id: albumId }, query: { reviewData } });
+        } else {
+          console.error('Invalid albumId:', albumId);
+          // Handle error (e.g., show error message)
+        }
+      } catch (error) {
+        console.error('Error fetching review:', error);
+        // Handle error (e.g., show error message)
+      }
+    }
+  }
 };
 </script>
 
 <style scoped>
+.album {
+  cursor: pointer; /* Change cursor to pointer */
+}
 
+/* Add hover effect */
+.album:hover {
+  transition: all 0.3s;
+  opacity: 0.8; /* Change opacity when hovering */
+}
 </style>
