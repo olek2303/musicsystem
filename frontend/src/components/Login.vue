1ed7ce749ed29cbe61f24fbc1a7023e6605483c2
olek2303 <olek2303@gmail.com>
2024-05-13 19:32:25 +0200

    working register and authentication, but login has 403 error, need to repair

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="w-full items-center flex flex-col">
+    <h2 class="pb-2">Login</h2>
+    <form @submit.prevent="login" class="grid grid-cols-1">
+      <label class="pb-1">
+        Email:
+        <input v-model="user.email" type="email" required>
+      </label>
+      <label class="pb-1">
+        Password:
+        <input v-model="user.password" type="password" required>
+      </label>
+      <button type="submit">Log In</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      user: {
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    async login() {
+      console.log('login method called')
+      console.log('user data:', this.user)
+      try {
+        const response = await axios.post('http://localhost:8080/api/v1/auth/authenticate', this.user)
+        console.log('response:', response)
+      } catch (error) {
+        console.error('error:', error)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* Add your CSS styles for the login form here */
+</style>
