18fd1a279e668c04e96338629b32f15e6f688edd
olek2303 <olek2303@gmail.com>
2024-05-14 19:22:31 +0200

    login works now

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,22 +1,25 @@
 <template>
-  <div class="w-full items-center flex flex-col">
-    <h2 class="pb-2">Login</h2>
-    <form @submit.prevent="login" class="grid grid-cols-1">
-      <label class="pb-1">
-        Email:
-        <input v-model="user.email" type="email" required>
-      </label>
-      <label class="pb-1">
-        Password:
-        <input v-model="user.password" type="password" required>
-      </label>
-      <button type="submit">Log In</button>
-    </form>
+  <div class="sm:h-96 h-fit justify-center">
+    <div class="w-full items-center flex flex-col">
+      <h2 class="pb-2">Login</h2>
+      <form @submit.prevent="login" class="grid grid-cols-1">
+        <label class="pb-1">
+          Email:
+          <input v-model="user.email" type="email" required>
+        </label>
+        <label class="pb-1">
+          Password:
+          <input v-model="user.password" type="password" required>
+        </label>
+        <button type="submit">Log In</button>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import router from '../router'
 
 export default {
   data() {
@@ -34,6 +37,8 @@ export default {
       try {
         const response = await axios.post('http://localhost:8080/api/v1/auth/authenticate', this.user)
         console.log('response:', response)
+        window.alert('Login successful.')
+        router.push('/')
       } catch (error) {
         console.error('error:', error)
       }
