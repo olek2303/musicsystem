a58b433dbf88f1d150f0caf5542f60e9eeac99ed
olek2303 <olek2303@gmail.com>
2024-05-28 11:55:06 +0200

    working with pipelines

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig, rollupVersion} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
@@ -8,6 +8,13 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  build: {
+    rollupVersion: {
+      input: {
+        main: resolve('frontend', 'index.html')
+      }
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
