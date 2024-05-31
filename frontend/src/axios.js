9ce6eb28c4ee7a54487e9c6f765b2221c6e89528
olek2303 <olek2303@gmail.com>
2024-05-31 23:49:53 +0200

    fix for frontend

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -5,7 +5,8 @@ console.log('API URL:', import.meta.env.VITE_API_URL);
 const instance = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
     headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
     }
 });
 
