e904981cd1ccc15a5246e86fc7dd769d72c207c4
olek2303 <olek2303@gmail.com>
2024-06-03 23:17:02 +0200

    changes to backend

--- musicsystem/src/main/java/dev/musicsystem/musicsystem/security/SecurityConfig.java
+++ musicsystem/src/main/java/dev/musicsystem/musicsystem/security/SecurityConfig.java
@@ -2,10 +2,8 @@ package dev.musicsystem.musicsystem.security;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,7 +12,6 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -41,41 +38,41 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.setAllowedOrigins(Arrays.asList(allowedOrigins));
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-            http
-                    .cors(withDefaults())
-                    .csrf(AbstractHttpConfigurer::disable)
-                    .authorizeHttpRequests(req -> req
-                            .requestMatchers("/api/v1/auth/**",
-                                    "/api/v1/albums/**",
-                                    "/api/v1/reviews/**",
-                                    "/api/v1/auth/register",
-                                    "/api/v1/auth/authenticate",
-                                    "/api/v1/v2/api-docs",
-                                    "/api/v1/v3/api-docs",
-                                    "/api/v1/v3/api-docs/**",
-                                    "/api/v1/configuration/ui",
-                                    "/api/v1/swagger-resources",
-                                    "/api/v1/swagger-resources/**",
-                                    "/api/v1/configuration/ui",
-                                    "/api/v1/configuration/security",
-                                    "/api/v1/swagger-ui/**",
-                                    "/api/v1/swagger-ui/index.html#/",
-                                    "/api/v1/webjars/**",
-                                    "/api/v1/swagger-ui.html").permitAll()
-                            .anyRequest().authenticated()
-                    )
-                    .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                    .authenticationProvider(authenticationProvider)
-                    .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-            return http.build();
+        http
+                .cors(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req -> req
+                        .requestMatchers("/api/v1/auth/**",
+                                "/api/v1/albums/**",
+                                "/api/v1/reviews/**",
+                                "/api/v1/auth/register",
+                                "/api/v1/auth/authenticate",
+                                "/v2/api-docs",
+                                "/v3/api-docs",
+                                "/v3/api-docs/**",
+                                "/configuration/ui",
+                                "/swagger-resources",
+                                "/swagger-resources/**",
+                                "/configuration/ui",
+                                "/configuration/security",
+                                "/swagger-ui/**",
+                                "/swagger-ui/index.html#/",
+                                "/webjars/**",
+                                "/swagger-ui.html").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
     }
 
 }
