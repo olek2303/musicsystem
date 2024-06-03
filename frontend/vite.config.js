21b5d486884581c0f61e3f66f95008fb9a51c27c
olek2303 <olek2303@gmail.com>
2024-06-03 23:35:11 +0200

    changed front

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,27 +1,29 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import {defineConfig, rollupVersion} from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig, loadEnv } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  // build: {
-  //   rollupVersion: {
-  //     input: {
-  //       main: resolve('index.html')
-  //     }
-  //   },
-  // },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  const env = loadEnv(mode, process.cwd(), '');
+
+  console.log('VITE_API_URL:', env.VITE_API_URL);
+
+  return {
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      host: true,
+      port: 8080
+    },
+    define: {
+      'process.env': env
     }
-  },
-  server: {
-    host: true,
-    port: 8080
-  }
-})
+  };
+});
